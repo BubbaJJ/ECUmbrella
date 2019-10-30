694cc38048603ce7b097122431ba04048de69dfa
pontus <pontus@bodelind.se>
2019-10-30 15:32:40 +0100

    stuff

---
+++ Umbrella_Theaters_backend/Models/SearchMoviePaged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Umbrella_Theaters_backend.Models
+{
+	public class SearchMoviePaged
+	{
+        public int Page { get; set; }
+        public int TotalResults { get; set; }
+        public int TotalPages { get; set; }
+        public List<Movie> Movies { get; set; }
+	}
+}
