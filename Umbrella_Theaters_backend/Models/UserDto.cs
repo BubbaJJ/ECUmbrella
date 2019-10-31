75b82c9c6cc39c43265a94cac6613f5a4c603da3
pontus <pontus@bodelind.se>
2019-10-31 14:17:25 +0100

    userdto

---
+++ Umbrella_Theaters_backend/Models/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Umbrella_Theaters_backend.Models
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Password { get; set; }
+
+    }
+}
